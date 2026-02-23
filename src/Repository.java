fc57fc526b821acad1334f87dc717c47388f5858
vostries <hzz@ro.ru>
2026-02-23 11:52:45 +0000

    3.1 интерфейс репозитория: Repository<T> (add, remove, findById, findAll, count, clear)

---
+++ src/Repository.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Optional;
+
+interface Repository<T> {
+    void add(T item);
+
+    boolean remove(T item);
+
+    Optional<T> findById(String id);
+
+    List<T> findAll();
+
+    int count();
+
+    void clear();
+}
